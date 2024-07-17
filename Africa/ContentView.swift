21300fbc58c33ddc1bb6f515664287e3c1bd59ae
Varun <pvr994@gmail.com>
2024-07-17 10:41:45 +0530

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Raj.P, Varun (Cognizant) on 17/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
